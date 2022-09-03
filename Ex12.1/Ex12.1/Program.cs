1e502bdf56f2c705b7236054d9dc54ddf0963f08
edclay1992 <104108509+edclay1992@users.noreply.github.com>
2022-09-02 21:20:33 -0300

    Add files via upload

---
+++ Ex12.1/Ex12.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo;
+             triangulo = new Triangulo();
+
+            Console.Write("Digite o primeiro valor: ");
+            triangulo.setA(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o segundo valor: ");
+            triangulo.setB(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o terceiro valor: ");
+            triangulo.setC(int.Parse(Console.ReadLine()));
+
+            triangulo.Calcular();
+        }
+    }
+}
---
+++ Ex12.1/Ex12.1/Triangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12._1
+{
+    internal class Triangulo
+    {
+        private int a1;
+        private int b1;
+        private int c1;
+        private int resultado;
+
+        public void setA(int a)
+        { 
+            a1 = a; 
+        }
+        public void setB(int b)
+        {
+            b1 = b;
+        }
+        public void setC(int c)
+        { 
+            c1 = c; 
+        }
+        public int getA()
+        {
+            return a1; 
+        }
+        public int getB()
+        {
+            return b1;
+        }
+        public int getC()
+        {
+            return c1;
+        }
+        public int getResultado()
+        {
+            return resultado;
+        }
+        public void Calcular()
+        {
+            if (a1 + b1 > c1 && a1 + c1 > b1 && b1 + c1 > a1)
+                if (a1 == b1 && a1 == c1)
+                    Console.WriteLine("Triângulo Equilátero");
+                else
+                if (a1 == b1 || a1 == c1 || b1 == c1)
+                    Console.WriteLine("Triângulo Isósceles");
+                else
+                    Console.WriteLine("Triângulo Escaleno");
+            else
+                Console.WriteLine("Não Formam Triângulo");
+        }
+    }
+}
